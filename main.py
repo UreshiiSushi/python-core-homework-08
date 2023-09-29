10a9d51540c186201b93c2530b561bf6e95c63d7
UreshiiSushi <merieru@gmail.com>
2023-09-29 02:42:56 +0200

    starting homework

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+from collections import defaultdict
+
+
+def get_period(start_date: date, days: int):
+    result = {}
+    for _ in range(days+1):
+        result[start_date.day, start_date.month] = start_date.year
+        start_date += timedelta(1)
+    return result
 
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
-    return users
+    if not users:
+        return None
+    result = defaultdict([])
+    start_date = date.today()
+    period = get_period(start_date, 7)
+    for i in users:
+        bd = i["birthday"]
+        date_bd = bd.day, bd.month
+        if date_bd in list(period):
+            if 
+    if result:
+        return result
+    else:
+        None
 
 
 if __name__ == "__main__":
